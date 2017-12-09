d382bbf2748ed590987d53eb61c2f4a9dd5a4d32
Joe Brown <jbrown762@gmail.com>
2017-12-09 15:12:26 -0500

    form validation

--- public/js/script.js
+++ public/js/script.js
@@ -19,7 +19,77 @@ $(document).ready(function () {
 
     });
 
-    $('#btnInventory').on('click', function () {
+    $("[name='btnInventory']").on('click', function () {
         window.open('http://w1w024.financeexpress.com/clients/baymotors/inventory_list.aspx', '_blank');
     });
+
+
+
+
+    // var isSubmitClicked = false;
+    // var form = document.forms["contactForm"];
+    // var inputs = {
+    //     name: form["name"],
+    //     subject: form["subject"],
+    //     email: form["email"],
+    //     message: form["message"]
+    // }
+
+    // function addInputListener(input) {
+    //     var inputEl = inputs[input];
+    //     inputEl.addEventListener("input", function (event) {
+    //         checkValidity(input);
+
+    //     }, false);
+    // };
+
+    // function checkValidity(input) {
+    //     var inputEl = inputs[input];
+    //     if (isSubmitClicked) {
+    //         if (inputEl.validity.valid) {
+    //             console.log('removing error')
+    //             $(inputEl).removeClass('has-error');
+    //             $('#' + input + '-error-required').addClass('d-none')
+    //             return true;
+    //         } else {
+    //             $(inputEl).addClass('has-error');
+    //             $('#' + input + '-error-required').removeClass('d-none')
+    //             return false;
+    //         }
+    //     }
+    // };
+
+    // for (input in inputs) {
+    //     addInputListener(input);
+    // };
+
+    // form.addEventListener("submit", function (event) {
+    //     console.log("clicked")
+    //     event.preventDefault();
+
+    //     var isFormValid = true;
+    //     isSubmitClicked = true;
+
+    //     for (input in inputs) {
+    //         if (!checkValidity(input)) {
+    //             isFormValid = false;
+    //         }
+    //     };
+
+    //     if (isFormValid) {
+    //         console.log("submit form!!!")
+    //     }
+    // }, false);
+
+    var form = document.forms["contactForm"];
+    form.addEventListener('submit', function (event) {
+        event.preventDefault();
+        event.stopPropagation();
+
+        if (form.checkValidity() === true) {
+            console.log("clicked, send email")
+        }
+        
+        form.classList.add('was-validated');
+    }, false);
 });
